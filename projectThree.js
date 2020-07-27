c281249395b7ee39cbcc180d34676f920fd4e415
Sofia <sofia.lau.alves@gmail.com>
2020-07-27 00:06:00 -0400

    added more jequery

--- projectThree.js
+++ projectThree.js
@@ -6,8 +6,6 @@ $(document).ready(function() {
   console.log('document ready'); 
   // Call the init function when the document is ready
   timerApp.init ();
-  // Inputs will display 1 when page is refreshed
-  // $('input[type=number]').val('');
 })
 
   // 1. Create an object to organize my code in 
@@ -18,14 +16,18 @@ timerApp.init = function() {
   timerApp.eventListener (); 
 }
 
-  // 2. The form BUTTON will LISTEN for the form on "SUBMIT" event. 
+// 2. The form BUTTON will LISTEN for the form on "SUBMIT" event. 
 timerApp.eventListener = function() {
   $('form').on('submit', function(e){
-    // Prevent refresh on submit 
     e.preventDefault();
     // Grab the form and add an attribute of aria-label called timer started for A11y 
     $('form').attr('aria-label', 'timer started');
     $('button[type=submit]').addClass('submitted');
+    // Add border around the Total Work Out timer once submit is clicked to indicate entry has been set 
+    $('#totalWorkoutTimer').addClass('lightUpTotal');
+    // Disable users from submitting more than once. 
+    $('#disable').attr('disabled',true);
+
     // Add the value (placed into the first input) into a variable 
     const userInput1 = $('#typesOfExercises').val();
     // Convert the string into an integer (to be used for caluclations) and place it into a variable 
@@ -44,18 +46,21 @@ timerApp.eventListener = function() {
 
     // Call the totalTimer function and pass the parameter (total exercise time) in the function 
     timerApp.totalTimer (totalSecsOfExercises);
+   
   })
 }
 
 // 3. Create a variable to store the function of the total workout countdown timer  
 timerApp.totalTimer = function(totalSecsOfExercises) {
   let timer = totalSecsOfExercises;
-  // Display the total workout time on the DOM
+  // Display the total workout time on the DOM 
   $('.totalSeconds').text(timer);
-  
   let timeToBreak = 0;
   // Start timer witha 3 seconds delay 
   setTimeout(() => {
+    const startAudio = new Audio ('./assets/go-start.wav');
+    // "Go" Audio will play after 3 second delay
+    startAudio.play ();
     let timerInterval = setInterval(function (){
       console.log('tick')
       $('.totalSeconds').text(timer);
@@ -76,9 +81,10 @@ timerApp.totalTimer = function(totalSecsOfExercises) {
 }
 
 // 4. Create a variable to store the function of the break countdown timer 
-timerApp.breakTimer = function( ) {
+timerApp.breakTimer = function() {
   // Break Timer will start at 5 seconds 
   let timer = 5; 
+  $('#breakTimer').addClass('lightUpTotal');
   const breakAudio = new Audio ('./assets/5-sec-countdown.wav');
   // 5 second countown audio will play when break timer is called (Started)
   breakAudio.play ();
@@ -93,9 +99,17 @@ timerApp.breakTimer = function( ) {
     // When the break timer reaches 0, stop countdown
     if (timer === 0) {
         clearInterval(timerInterval);
+        // Remove border when at zero
+        $('#breakTimer').removeClass('lightUpTotal');
         // Continue to display the number to the DOM even if it is 0 
         $('.breakSeconds').text(timer);
     }
   }, 1000)
 }
 
+// 5. Preventing users from clicking submit more than once 
+// timerApp.clickOnce = function DisableButton(button) {
+//   button.disabled = true;
+//   button.value = 'Submitted'; 
+//   button.form.submit();
+// }
